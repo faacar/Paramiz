4ba548889d974e9f079d738be55eef68b897a966
Ahmet Acar <farukacar1998@gmail.com>
2020-10-01 17:31:45 +0300

    TableView scroll olunca checkmark kaymasi sorunu cozuldu

---
+++ Paramiz/Aktivite.swift
@@ -0,0 +1,14 @@
+//
+//  Aktivite.swift
+//  Paramiz
+//
+//  Created by Ahmet Acar on 30.09.2020.
+//  Copyright © 2020 Ahmet Acar. All rights reserved.
+//
+
+import Foundation
+
+class Aktivite {
+    var Adi: String = ""
+    var Bittimi: Bool = false
+}
--- Paramiz/AktivitelerVC.swift
+++ Paramiz/AktivitelerVC.swift
@@ -12,20 +12,47 @@ class AktivitelerVC: UITableViewController {
 //MARK: -TODO : uygulamaya hic veri girilmediyse nil oluyo ve uygulamanin cokmesine sebep oluyor ilk degeri bastan kendimiz atamaliyiz
     
     
-    var aktivitelerListesi = ["Ev", "Kapadokya Gezisi", "Istanbul gezisi", "Okul Arkadaslari"]
+    //var aktivitelerListesi = ["Ev", "Kapadokya Gezisi", "Istanbul gezisi", "Okul Arkadaslari"]
+    var aktivitelerListesi = [Aktivite]()
     var veriler = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        /*
         if let liste = veriler.array(forKey: "AktiviteListesi") as? [String] { // eger henuz hic deger girilmediyse uygulama crash olmasin diye yazdim
             //veriler.set("", forKey: "AktiviteListesi")
             aktivitelerListesi = liste
         }
         for i in 1...100 {
             aktivitelerListesi.append("\(i)")
+        }*/
+        let a1 = Aktivite()
+        a1.Adi = "Ev"
+        a1.Bittimi = true
+        aktivitelerListesi.append(a1)
+
+        let a2 = Aktivite()
+        a2.Adi = "Kapadokya gezisi"
+        aktivitelerListesi.append(a2)
+        
+        let a3 = Aktivite()
+        a3.Adi = "Istanbul gezisi"
+        a3.Bittimi = false
+        aktivitelerListesi.append(a3)
+        
+        let a4 = Aktivite()
+        a4.Adi = "Okul arkadaslari"
+        a4.Bittimi = true
+        aktivitelerListesi.append(a4)
+        
+        for i in 1...100 {
+            let a5 = Aktivite()
+            a5.Adi = "Okul arkadaslari\(i)"
+            a5.Bittimi = false
+            aktivitelerListesi.append(a5)
         }
-    
+        
+        
         //aktivitelerListesi = veriler.array(forKey: "AktiviteListesi") as! [String]
     }
 
@@ -40,19 +67,32 @@ class AktivitelerVC: UITableViewController {
     }
     //cellforrowat
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //let cell = UITableViewCell(style: .default, reuseIdentifier: "aktiviteCell")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "aktiviteCell", for: indexPath)
-        cell.textLabel?.text = aktivitelerListesi[indexPath.row]
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "aktiviteCell")
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "aktiviteCell", for: indexPath)
+        cell.textLabel?.text = aktivitelerListesi[indexPath.row].Adi
+   
+        if aktivitelerListesi[indexPath.row].Bittimi {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
-    //didselectrowat
+
+    
+    //didselectrowat -- satira basildiginda yapicalak islemler
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let secilenHucre = tableView.cellForRow(at: indexPath)
-        if secilenHucre?.accessoryType == .checkmark { // UITableViewCell.AccessoryType.checkmark bu sekilde de calisiyor
-            secilenHucre?.accessoryType = .none
-        } else {
-            secilenHucre?.accessoryType = .checkmark
-        }
+        
+        aktivitelerListesi[indexPath.row].Bittimi = !aktivitelerListesi[indexPath.row].Bittimi
+        
+//        if secilenHucre?.accessoryType == .checkmark { // UITableViewCell.AccessoryType.checkmark bu sekilde de calisiyor
+//            secilenHucre?.accessoryType = .none
+//        } else {
+//            secilenHucre?.accessoryType = .checkmark
+//        }
+        tableView.reloadData() // cellForRowAt metodu tekrar calisacak ve butun satirlar yeniden oluscak
     }
     
     @IBAction func btnAktiviteEkle(_ sender: UIBarButtonItem) {
@@ -64,7 +104,9 @@ class AktivitelerVC: UITableViewController {
             let txtAktiviteAdi = alertController.textFields![0]
             
             if !txtAktiviteAdi.text!.isEmpty { //herhangi bir aktivite girildiyse - bir deger girisi varsa
-                self.aktivitelerListesi.append(txtAktiviteAdi.text!)
+                let a1 = Aktivite()
+                a1.Adi = txtAktiviteAdi.text!
+                self.aktivitelerListesi.append(a1)
                 self.veriler.set(self.aktivitelerListesi, forKey: "AktiviteListesi")
                 
                 self.tableView.reloadData()
